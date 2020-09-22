18e57adfd1e3f01d06f6deb41e1aad20d5848bde
Josh Padilla <jdp237@nau.edu>
2020-09-22 14:22:01 -0700

    push many changes to javascript and html

--- script.js
+++ script.js
@@ -2,47 +2,98 @@ $(document).ready(function () {
 
     $("#date").text(moment().format("MMMM Do YYYY"));
 
+    let searchHistory = JSON.parse(window.localStorage.getItem("city")) || [];
 
-    $.ajax({
-        url: "http://api.openweathermap.org/data/2.5/weather?q=Phoenix&appid=c61145b4fc397c4d9a55e08f94847531",
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
+    $("#searchbtn").on("click", function (event) {
+        event.preventDefault();
+        let usersearch = $("#searchbar").val();
+        console.log(usersearch);
+        currentWeather(usersearch);
+        $("#searchbar").val("");
+        searchHistory.push(usersearch);
+        window.localStorage.setItem("city", JSON.stringify(searchHistory));
+        let newCity = $("<li>");
+        newCity.attr("class", "list-group-item");
+        let newCityBtn = $("<button>").text(usersearch).val(usersearch);
+        newCity.append(newCityBtn);
+        $("#citylist").append(newCity);
+    })
 
-        let temperature = response.main.temp;
-        // console.log(temperature);
-        $("#temp").text("Temperature: " + (((temperature - 273.15) * 1.8) + 32).toFixed(2) + "°F");
-        $("#humidity").text("Humidty: " + response.main.humidity + "%");
-        $("#city").text(response.name);
 
-        //UV index api call
-        let uvIndexUrl = "http://api.openweathermap.org/data/2.5/uvi?appid=c61145b4fc397c4d9a55e08f94847531&lat=" + response.coord.lat + "&lon=" + response.coord.lon;
+    for (i = 0; i < searchHistory.length; i++) {
+
+        let newCity = $("<li>");
+        let newCityBtn = $("<button>").text(searchHistory[i]).val(searchHistory[i]);
+        newCity.append(newCityBtn);
+        $("#citylist").append(newCity);
+
+    }
+
+$("#citylist").on("click", "button", function(event) {
+    event.preventDefault();
+    currentWeather($(this).val());
+
+})
+
+
+
+
+
+    function currentWeather(city) {
+        $.ajax({
+            url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=c61145b4fc397c4d9a55e08f94847531",
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+
+            let temperature = response.main.temp;
+            // console.log(temperature);
+            $("#temp").text("Temperature: " + (((temperature - 273.15) * 1.8) + 32).toFixed(2) + "°F");
+            $("#humidity").text("Humidty: " + response.main.humidity + "%");
+            $("#city").text(response.name);
+
+            uvIndex(response.coord.lat, response.coord.lon);
+            fiveDayForecast(city);
+        });
+    };
+
+    function uvIndex(lat, lon) {
+        //UV index api call 
+        let uvIndexUrl = "http://api.openweathermap.org/data/2.5/uvi?appid=c61145b4fc397c4d9a55e08f94847531&lat=" + lat + "&lon=" + lon;
         // console.log(uvIndexUrl);
         $.ajax({
             url: uvIndexUrl,
             method: "GET"
         }).then(function (uvResponse) {
-            // console.log(uvResponse);
+
+            //add if else statement for styling page for background color
+            console.log(uvResponse);
             $("#UV").text("UV Index: " + uvResponse.value)
 
         })
 
+    };
+
+    function fiveDayForecast(city) {
         //5 day forecast api call
         $.ajax({
-            url: "http://api.openweathermap.org/data/2.5/forecast?q=Phoenix&appid=c61145b4fc397c4d9a55e08f94847531",
+            url: "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=c61145b4fc397c4d9a55e08f94847531",
             method: "GET"
         }).then(function (fiveDayResponse) {
             console.log(fiveDayResponse);
+            let filteredArray = fiveDayResponse.list.filter((time) => {
+                return time.dt_txt.includes("15:00:00");
+            });
+            console.log(filteredArray);
 
+            for (let i = 0; i < filteredArray.length; i++) {
+
+                console.log(filteredArray[i].main.temp);
+
+            }
         })
 
-
-    });
-
-
-
-
-
+    };
 
 
 })
